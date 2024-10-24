243c7a525821864eb6751c4780da2dec40b4fa41
aroushz <aroushsyeda05@gmail.com>
2024-10-24 13:31:36 +0200

    player screen

---
+++ PlayerScreen.java
@@ -0,0 +1,14 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JPanel;
+
+public class PlayerScreen {
+
+    public MainFrame parentFrame; //parent frame for panel switching functions
+    private Image bg = new ImageIcon("resources/ChoosePlayers.jpg").getImage();
+    
+
+}
