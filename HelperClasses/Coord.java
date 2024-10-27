95659b5e9604900dfe8ceffa6812ddecb8894c8d
aroushz <aroushsyeda05@gmail.com>
2024-10-27 23:31:40 +0100

    Javadoc comments added

--- HelperClasses/Coord.java
+++ HelperClasses/Coord.java
@@ -1,9 +1,19 @@
 package HelperClasses; 
 
+/**
+ * The Coord Class.
+ * Creates coordinates which are used to faciliate drawing levels in the level builder 
+ * 
+ */
 public class Coord {
     public final int x;
     public final int y;
 
+    /**
+     * The contructor for the coordinate class. 
+     * @param x x position of the coordinate
+     * @param y y postion of the coordinate
+     */
     public Coord(int x, int y) {
         this.x = x;
         this.y = y;
@@ -11,8 +21,12 @@ public class Coord {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        } 
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Coord that = (Coord) o;
         return x == that.x && y == that.y;
     }
--- HelperClasses/KeyChecker.java
+++ HelperClasses/KeyChecker.java
@@ -5,14 +5,28 @@ import PlayerCharacters.FirstPlayer;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class KeyChecker extends KeyAdapter{
+/**
+ * The keyChecker class is-a keyAdapter.
+ * Handles key input for player movement
+ * Differentiates between player one which used WASD keys 
+ * and player two which uses arrow keys 
+ * 
+ */
+public class KeyChecker extends KeyAdapter {
 
     BoxPeople player;
     Boolean isPlayerOne;
 
-    public KeyChecker (BoxPeople player) {
+    /**
+     * KeyChecker contructor. 
+
+     * 
+     * @param player an instance of the box people class representing the player
+     *               determines which control scheme to use.
+     */
+    public KeyChecker(BoxPeople player) {
         this.player = player;
-        if(player instanceof FirstPlayer){
+        if (player instanceof FirstPlayer) {
             isPlayerOne = true;
         } else {
             isPlayerOne = false;
@@ -20,7 +34,12 @@ public class KeyChecker extends KeyAdapter{
         
     }
 
-    private void keySwitch(KeyEvent e, boolean isPress){
+    /**
+     * The key switch method.
+     * @param e the keyEvent representing the key action
+     * @param isPress a boolean indicating wether a key is pressed (true) or released (false)
+     */
+    private void keySwitch(KeyEvent e, boolean isPress) {
         if (isPlayerOne) {
             switch (e.getKeyChar()) {
                 case 'w':
